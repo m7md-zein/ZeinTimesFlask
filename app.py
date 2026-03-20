2e8bb450dcc8e68bedb7d94443311c66bb6689fa
m7md-zein <aaalava70@gmail.com>
2026-03-20 08:18:58 +0200

    fix: add Procfile and requirements.txt

--- app.py
+++ app.py
@@ -477,4 +477,4 @@ def unfollow(newspaper_id):
     return redirect(request.referrer or url_for("home"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
